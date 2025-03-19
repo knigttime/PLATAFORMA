51bb62c0ea2c836c675e2d4e9ae70a9b94618e0e
Inveta pro <leonardorinconcastro3@gmail.com>
2025-03-19 08:16:15 -0500

    subiendo

---
+++ core/configGeneral.php
@@ -0,0 +1,23 @@
+<?php
+	const SERVERURL = "http://localhost/plataforma/";
+
+	const COMPANY = "PLATAFORMA";
+
+	/*====================================
+	=            Zona horaria            =
+	====================================*/
+	date_default_timezone_set("America/Bogota");
+
+	/**
+		Zonas horarias:
+		- America/El_Salvador
+		- America/Costa_Rica
+		- America/Guatemala
+		- America/Puerto_Rico
+		- America/Panama
+		- Europe/Madrid
+		-America/Bogota
+
+		Más zonas, visita http://php.net/manual/es/timezones.php
+
+	/*=====  End of Zona horaria  ======*/
